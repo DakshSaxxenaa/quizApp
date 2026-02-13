7cad7a8b4e12acaf41aaa0761e60000f8bc4ea3c
unknown <dakshsaxxenaa@gmail.com>
2026-02-13 22:10:31 +0530

    bg image downloaded

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import { BACKGROUND_URL } from "./utils/constants.js";
+// import { BACKGROUND_URL } from "./utils/constants.js";
+import BACKGROUND_URL from "./assets/landing_cover_latest.jpg"
 import { Outlet } from "react-router-dom";;
 function App() {
   return (
